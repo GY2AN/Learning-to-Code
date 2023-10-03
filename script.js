136e8c4db0c54f8c520bef55e74f08fb33561760
Gyan <pgyan4224@gmail.com>
2023-10-03 05:37:42 +0530

    Added some js code

---
+++ script.js
@@ -0,0 +1,46 @@
+let gameRes = document.getElementById("gameResult");
+let mathRandomOne,mathTwoRandom;
+
+function reset(){
+    mathRandomOne = Math.ceil(Math.random() * 10);
+    mathTwoRandom = Math.floor(Math.random() * 50);
+    let firstNumber = document.getElementById("firstNumber");
+    firstNumber.textContent = mathRandomOne;
+    let secondNumber = document.getElementById("secondNumber");
+    secondNumber.textContent = mathTwoRandom;
+    document.getElementById("userNum").value= "";
+    gameRes.textContent = "Baby Suno na, Please Enter the Sum";
+    gameRes.style.color = "orange";
+}
+
+function check(){
+    let userInput = document.getElementById("userNum");
+    let userInputValue = parseInt(userInput.value);
+    let numMathRandomOne = parseInt(mathRandomOne);
+    let numMathTwoRandom = parseInt(mathTwoRandom);
+    let totalSum  = numMathRandomOne + numMathTwoRandom;
+    //let a = Calc(userInputValue);
+    
+
+    if( totalSum === userInputValue){
+        document.getElementById("gameResult").textContent = "I trust you baby";
+        document.getElementById("gameResult").style.color = "green";
+    }else{
+       gameRes.textContent = "Anpadh baby, Babu Madarchod";
+       gameRes.style.color = "red";
+    }
+}
+
+/*function check(){
+
+    if(totalSum === userInputValue){
+        document.getElementById("gameResult").textContent = "That's correct!!!";
+        document.getElementById("gameResult").style.color = "green";
+    }else{
+       gameRes.textContent = "Try again!!!";
+       gameRes.style.color = "red";
+    }
+}*/
+
+
+
